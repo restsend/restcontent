bd66f384b7f4a8a6ab14bd1d87939b6412e7db4b
jinmac <shenjindi@fourz.cn>
2023-11-20 16:51:54 +0800

    update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,9 @@
 #!/bin/sh
 ADDR=0.0.0.0:${PORT:=8080}
-
-if [ -z "$DSN" ]; then
-    DSN="file:data/restcontent.db"
-fi
-
 # output with logfile 
 if [ -n "$LOGFILE" ]; then
     LOG="-log $LOGFILE"
 fi
 
 set -x 
-SESSION_SECRET=$SESSION_SECRET ./restcontent -addr $ADDR -dsn $DSN $LOG $@
+./restcontent -addr $ADDR $LOG $@
